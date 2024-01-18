736b1d1dafdf34c96c997b7bee6f5abfd7242a3d
AustinK <125139752+NeoMatrixafk@users.noreply.github.com>
2024-01-18 21:45:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 import store, { persistor } from "./Redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import ScrollToTop from "./ScrollToTop";
@@ -16,10 +16,10 @@ root.render(
     <React.StrictMode>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <BrowserRouter>
+                <HashRouter>
                     <ScrollToTop />
                     <App />
-                </BrowserRouter>
+                </HashRouter>
             </PersistGate>
         </Provider>
     </React.StrictMode>
